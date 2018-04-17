849e0ae57321e9c306ba8b67b7302ea6053b2d38
老黄 <huangchengshi@huangchengshideimac.local>
2018-04-17 10:08:59 +0800

    Initial Commit

---
+++ framework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  framework
+//
+//  Created by 黄成实 on 2018/4/17.
+//  Copyright © 2018年 黄成实. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
