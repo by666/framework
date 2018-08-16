8a9c192b06430bf26a3a8311f7e5eb13313d4afa
by.huang <chengshi.huang@cootek.cn>
2018-08-16 14:12:30 +0800

    move files

---
+++ framework/viewcontroller/face/FaceEnterPage2.h
@@ -0,0 +1,18 @@
+//
+//  FaceBaseViewController.h
+//  IDLFaceSDKDemoOC
+//
+//  Created by 阿凡树 on 2017/5/23.
+//  Copyright © 2017年 Baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+
+@interface FaceEnterPage2 : BaseViewController
+
++(void)show:(BaseViewController *)controller;
+
+
+@end
